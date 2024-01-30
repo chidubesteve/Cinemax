ec6783d82dc62035e20d5c6d4859df52c8762620
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2024-01-30 14:44:00 +0100

    updated dependencies versions to prevent app crashes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 
-const App = () => {
-  <div>App</div>;
-};
+const App = () => (
+  <div>
+    <main>
+      <CssBaseline />
+      <Routes>
+        <Route path="/">
+          <h1>Home</h1>
+        </Route>
+        <Route path="/movies">
+          <h1>Movies</h1>
+        </Route>
+        <Route path="/movies/:id">
+          <h1>Movies</h1>
+        </Route>
+      </Routes>
+    </main>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import { ReactDOM } from 'react-dom/client';
+/* eslint-disable import/no-named-as-default-member */
+import React, { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
+// eslint-disable-next-line import/no-named-as-default
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
+    <BrowserRouter future={{ v7_startTransition: true }}>
+      <App />,
+    </BrowserRouter>,
+  </StrictMode>,
+);
