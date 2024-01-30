87db71cf60e7c8276eb2b8be48f0d58037de48f4
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2024-01-31 00:50:35 +0100

    added mui styles and .env to gitignore - created .env.example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,33 +3,41 @@ import { CssBaseline } from '@mui/material';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Profile, Actors, MovieInformation, NavBar, Movies } from '.';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Movies />,
-  },
-  {
-    path: '/profile/:id',
-    element: <Profile />,
-  },
-  {
-    path: '/movies/:id',
-    element: <MovieInformation />,
-  },
-  {
-    path: '/actors/:id',
-    element: <Actors />,
-  },
-]);
+import useStyles from './styles';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <NavBar />
-    <main>
-      <RouterProvider router={router} />
-    </main>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Movies />,
+    },
+    {
+      path: '/profile/:id',
+      element: <Profile />,
+    },
+    {
+      path: '/movies/:id',
+      element: <MovieInformation />,
+    },
+    {
+      path: '/actors/:id',
+      element: <Actors />,
+    },
+  ]);
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+
+        <RouterProvider router={router} />
+      </main>
+    </div>
+  );
+};
 
 export default App;
