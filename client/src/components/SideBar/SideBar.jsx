07a79a33ad7073e37b7a0e42fed01dbacc9db806
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2024-05-21 14:08:48 +0100

    refactored jsx to center error message

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -9,29 +9,31 @@ import {
   ListItemIcon,
   ListSubheader,
   ListItemButton,
+  Typography,
 } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
+import { MdErrorOutline } from 'react-icons/md';
 
 // internal imports
 import useStyles from './styles';
 import logo from '../../assests/Cinemax.png';
+import { useGetGenresQuery } from '../../services/TMDB';
 
 const categories = [
   { label: 'Trending', value: 'trending' },
   { label: 'Top Rated', value: 'top_rated' },
   { label: 'Upcoming', value: 'upcoming' },
-];
-const demoCategories = [
-  { label: 'Horror', value: 'horror' },
-  { label: 'Animation', value: 'animation' },
-  { label: 'Comedy', value: 'comedy' },
-  { label: 'Action', value: 'action' },
+  { label: 'Now Playing', value: 'now_playing' },
 ];
 
 const SideBar = ({ setSideBarOpen }) => {
   const theme = useTheme();
   const classes = useStyles();
+
+  const { data, isLoading, isFetching, error } = useGetGenresQuery();
+  console.log(data);
+  
   return (
     <>
       <Link to={'/'} className={classes.logoLink}>
@@ -42,18 +44,8 @@ const SideBar = ({ setSideBarOpen }) => {
         <ListSubheader>Categories</ListSubheader>
         {categories.map(({ label, value }) => (
           <Link key={value} className={classes.links} to="/">
-            <ListItem onClick={() => {}} >  {/* disablePadding */}
+            <ListItem onClick={() => setSideBarOpen(false)}>
               <ListItemButton>
-                {/* <ListItemIcon>
-                  <img
-                    src={
-                      'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.44546679.1715731200&semt=ais_user'
-                    }
-                    alt="genre icons"
-                    className={classes.genreImages}
-                    height={30}
-                  />
-                </ListItemIcon> */}
                 <ListItemText primary={label} />
               </ListItemButton>
             </ListItem>
@@ -61,28 +53,44 @@ const SideBar = ({ setSideBarOpen }) => {
         ))}
       </List>
       <Divider />
-      <List>
-        <ListSubheader>Genres</ListSubheader>
-        {demoCategories.map(({ label, value }) => (
-          <Link key={value} className={classes.links} to="/">
-            <ListItem onClick={() => {}}>
-              <ListItemButton>
-                {/* <ListItemIcon>
-                  <img
-                    src={
-                      'https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.44546679.1715731200&semt=ais_user'
-                    }
-                    alt="genre icons"
-                    className={classes.genreImages}
-                    height={30}
-                  />
-                </ListItemIcon> */}
-                <ListItemText primary={label} />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-        ))}
-      </List>
+      {error ? (
+        <Box
+          marginBlock="auto"
+          height="100%"
+          display="flex"
+          alignItems="center"
+        >
+          <Typography variant="body1" textAlign="center" color="gray">
+            An error occurred while fetching genres <MdErrorOutline />
+          </Typography>
+        </Box>
+      ) : isFetching || isLoading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100%"
+        >
+          <CircularProgress
+            size="2rem"
+            style={{ color: '#E3E6E8' }}
+            thickness={2.8}
+          />
+        </Box>
+      ) : (
+        <List>
+          <ListSubheader>Genres</ListSubheader>
+          {data.genres.map(({ name, id }) => (
+            <Link key={id} className={classes.links} to="/">
+              <ListItem onClick={() => setSideBarOpen(false)}>
+                <ListItemButton>
+                  <ListItemText primary={name} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      )}
     </>
   );
 };
