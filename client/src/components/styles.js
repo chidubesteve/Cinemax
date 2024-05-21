1b3e10ee8bbc208e739f7612024bbb2998b6c3e1
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2024-05-21 14:06:29 +0100

    added global styles for scrollbar

--- client/src/components/styles.js
+++ client/src/components/styles.js
@@ -1,6 +1,23 @@
 import { makeStyles } from '@mui/styles';
 
 export default makeStyles(() => ({
+  '@global': {
+    '*::-webkit-scrollbar': {
+      width: '0.5rem',
+    },
+    '*::-webkit-scrollbar-track': {
+      '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
+      backgroundColor: ' #424242',
+    },
+    '*::-webkit-scrollbar-thumb': {
+      backgroundColor: '#7B7B7B',
+      borderRadius: '1.5rem',
+      width: '0.35rem',
+    },
+    '*::-webkit-scrollbar-thumb:hover': {
+      backgroundColor: '#E3E6d4',
+    },
+  },
   root: {
     display: 'flex',
     height: '100%',
@@ -11,5 +28,6 @@ export default makeStyles(() => ({
   content: {
     flexGrow: '1',
     padding: '2em',
+    height: '100dvh',
   },
 }));
