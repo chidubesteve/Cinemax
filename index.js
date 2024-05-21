fa25db8ca317cde0a86fe576db9a1ffa7efe3003
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2024-05-21 14:05:24 +0100

    Making api call to get genres

--- index.js
+++ index.js
@@ -20,32 +20,50 @@ app.use(express.json());
 app.use(limiter);
 app.use(cors());
 
+// creating a axios default instance
+const tmdbApi = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${TMDB_RAT}`,
+  },
+})
+
 // Proxy endpoint to get movies
 app.get('/api/movies', async (req, res) => {
-  const url = 'https://api.themoviedb.org/3/trending/movie/week';
   const params = {
     language: 'en-US',
     page: req.query.page || 1,
   };
-  const headers = {
-    accept: 'application/json',
-    Authorization: `Bearer ${TMDB_RAT}`,
-  };
 
-  axios
-    .get(url, {
-      params,
-      headers,
+  tmdbApi
+    .get('trending/movie/week', {
+      params
     })
     .then((result) => {
       res.status(200).send(result.data);
-      console.log(result.data);
     })
     .catch((error) => {
       console.log('Error fetching movies:', error.message);
       res.status(500).send({ message: 'Error fetching movies' });
     });
 });
+
+// proxy endpoint to get genres
+app.get('/api/genre/movie/list', async (req, res) => {
+  const params = {
+    language: 'en-US',
+  }
+
+  tmdbApi.get('genre/movie/list', {params}).then(result => {
+    res.status(200).send(result.data);
+    console.log(result.data);
+  }).catch(err => {
+    console.log('Error fetching genres:', err.message);
+    res.status(500).send({ message: 'Error fetching genres' });
+
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
