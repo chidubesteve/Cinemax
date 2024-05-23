77dd79240fd9d06bc80a122d8482c40716ac6b5f
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2024-05-23 13:19:43 +0100

    Added the reducer for genre or   category selection and listener for dispatch

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,17 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query'
+import { setupListeners } from '@reduxjs/toolkit/query';
 import { tmdbApi } from '../services/TMDB';
+import genreOrCategoryReducer from '../features/currentGenreOrCategory';
 
 const store = configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
+    currentGenreOrCategory: genreOrCategoryReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(tmdbApi.middleware),
 });
 
-setupListeners(store.dispatch)
+setupListeners(store.dispatch);
 
-export default store
-
-
+export default store;
