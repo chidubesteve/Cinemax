95e16c8459251ee237d68289cb15afa0700b60c4
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2024-06-02 23:10:38 +0100

    added noOfMovies prop coming from MovieInfo component

--- client/src/components/MovieList/MovieList.jsx
+++ client/src/components/MovieList/MovieList.jsx
@@ -3,14 +3,14 @@ import Grid from '@mui/material/Unstable_Grid2';
 import useStyles from './styles.js';
 import { Movie } from '..';
 
-const MovieList = ({ movies }) => {
+const MovieList = ({ movies, noOfMovies }) => {
   const classes = useStyles();
   return (
     <Grid container className={classes.moviesContainer}>
-      {movies.results.map((movie, i) => (
+      {movies.results.slice(0, noOfMovies).map((movie, i) => (
         <Movie key={i} movie={movie} i={i} />
       ))}
-    </Grid>
+    </Grid>   
   );
 };
 
